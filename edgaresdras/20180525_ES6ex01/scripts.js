fb183390f0f04caff6be3a01123644d19b286154
esdrasgreco <esdras_greco@yahoo.com>
2018-05-31 17:48:44 -0500

    notas bower y npm

--- edgaresdras/20180525_ES6ex01/scripts.js
+++ edgaresdras/20180525_ES6ex01/scripts.js
@@ -23,19 +23,21 @@ const btn = document.getElementById('myBtn');
 const span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = () => modal.style.display = 'block';
+btn.onclick = () => {
+  modal.style.display = 'block';
+};
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = () => modal.style.display = 'none';
+span.onclick = () => {
+  return modal.style.display = 'none';
+};
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
-}
+span.onclick = () => {
+  modal.style.display = 'none';
+};
 
-//Esta arrow function me marca error en el if
-// window.onclick = (event) => if (event.target === modal) {
-//   modal.style.display = 'none';
-//   }
+window.onclick = (event) => {
+  if (event.target === modal) {
+      modal.style.display = 'none';
+  }
+};
